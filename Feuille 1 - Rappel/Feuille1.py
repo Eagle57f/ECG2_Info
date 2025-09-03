27047703f99a7a49a956c3a83c5237f5e1026ee5
Eagle57 <94141086+Eagle57f@users.noreply.github.com>
2025-09-03 17:52:29 +0200

    Suite Feuille1

--- Feuille 1 - Rappel/Feuille1.py
+++ Feuille 1 - Rappel/Feuille1.py
@@ -21,6 +21,17 @@ print(f(range(30))) # Exemple pour la liste des entiers entre 0 et 29
 
 # 3)
 
+# Version "normale":
+
+def f(n,c):
+    S=0
+    for i in range(len(n)):
+        S+=n[i]*c[i]
+    return S/len(n)
+print(f([19,12,7,20], [1,2,4,1]))
+
+# Version en une ligne (difficle à comprendre):
+
 def f(n,c):
     return sum([i*j for i,j in zip(n,c)])/len(n)
 print(f([19,12,7,20], [1,2,4,1]))
@@ -57,6 +68,54 @@ print(f([1,2,3,4,5,4,3,2,1,2,3,4,5,1,1,1,1]))
 
 # 8)
 
+# Version "normale":
+
 def f(l):
-    return list(dict(zip(l,range(len(l))))), dict(zip(l,range(len(l))))
+    l2=[]
+    for i in l:
+        if i not in l2:
+            l2.append(i)
+    return l2
 print(f([1,2,3,"t", "ttt", "tttttt", 1, 2, 3, "t", "ttt"]))
+
+# Version en une ligne (difficle à comprendre, mais très efficace):
+
+def f(l):
+    return list(dict(zip(l,range(len(l)))))
+print(f([1,2,3,"t", "ttt", "tttttt", 1, 2, 3, "t", "ttt"]))
+
+# 9)
+
+def f(l):
+    x,y=l[0],l[1]
+    for i in l:
+        for j in l:
+            if abs(i-j)<abs(x-y) and i!=j:
+                x,y=i,j
+    return x,y
+print(f([1,3,6,8,10,15,18,20,25,30,35,40,45,50]))
+
+
+# 10)
+def f(n):
+    S=0
+    for i in range(n+1):
+        for j in range(n+1):
+            if i<j:
+                S+=i+j
+    return S
+print(f(4))
+
+# II. Quelques extraits de concours
+
+# Ex 1:
+from math import factorial
+import matplotlib.pyplot as plt
+
+n=100
+
+def u(n):
+    return factorial(n)/(n**n)
+
+plt.plot([u(i) for i in range(0,n+1)], range(0,n+1))
+plt.show()
